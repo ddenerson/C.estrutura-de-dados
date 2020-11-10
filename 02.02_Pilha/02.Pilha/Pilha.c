1e4d8e55c9e7d03e3cc943c09a2dfe865360a3b2
ddenerson <ddenerson@gmail.com>
2020-11-09 21:26:24 -0300

    past 02  Pilha

---
+++ 02.02_Pilha/02.Pilha/Pilha.c
@@ -0,0 +1,167 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct pilha
+{
+    int num;
+    struct pilha *prox;
+};
+typedef struct pilha pilha;
+pilha*top;
+
+struct fila
+{
+    int num;
+    struct fila *prox;
+};
+
+struct fila *topo = NULL;
+struct fila *corpo = NULL;
+
+void iniciar()
+{
+    top = NULL;
+}
+
+void entrada(int valor)
+{
+    pilha *tmp;
+    tmp = malloc(sizeof(pilha));
+    tmp -> num = valor;
+    tmp -> prox = top;
+    top = tmp;
+}
+
+int deletar_pilha()
+{
+    pilha *tmp;
+    int n;
+    tmp = top;
+    n = tmp->num;
+    top = top->prox;
+    free(tmp);
+    return n;
+}
+
+void mostrar_pilha(struct pilha*topo)
+{
+    if(topo == NULL)
+    {
+        printf("");
+    }
+    else
+    {
+        //Chamando recursivamente
+        printf("| %d | \n", topo->num);
+        mostrar_pilha(topo->prox);
+    }
+}
+
+//inserir no comeco da lista
+void inserir_fila(int dados)
+{
+    //criando o link
+    struct fila *link = (struct fila*) malloc(sizeof(struct fila));
+
+    link->num = dados;
+
+    //apontando para o primeiro no
+    link->prox = topo;
+
+    //apontando o primeiro no,para um novo no
+    topo = link;
+}
+
+
+void imprimirLista()
+{
+    struct fila *ptr = topo;
+    printf("\n");
+
+    // imprimindo a lista desde o começo
+    while(ptr != NULL)
+    {
+        printf(" | %d | ",ptr->num);
+        ptr = ptr->prox;
+    }
+
+}
+
+struct fila*deletar_fila()
+{
+
+    // referencia para o primeiro link
+    struct fila *link = topo;
+
+    // marcando ao lado do primeiro link como o primeiro
+    topo = topo->prox;
+
+    //retornando o numero deletado
+    return link;
+}
+
+
+int main()
+{
+    int opc = 0;
+    int v = 0;
+    iniciar();
+    do
+    {
+        system("clear");
+        printf("\n PILHA ");
+        printf("\n ----------");
+        printf("\n");
+        mostrar_pilha(top);
+        printf("\n");
+        printf("\n FILA ");
+        printf("\n ----------");
+        imprimirLista();
+
+
+        printf("\n");
+        printf("\n 1 - Empilhar");
+        printf("\n 2 - Desempilhar");
+        printf("\n 3 - Enfileirar");
+        printf("\n 4 - Desenfileirar");
+        printf("\n 5 - Sair");
+        printf("\n Escolha um numero :");
+        fflush(stdin);
+        scanf("%d",&opc);
+
+        switch(opc)
+        {
+        case 1:
+            printf("\n [EMPILHAR]");
+            printf("\n VALOR:");
+            fflush(stdin);
+            scanf("%d",&v);
+            entrada(v);
+            break;
+        case 2:
+            printf("\n [DESEMPILHAR]");
+            v = deletar_pilha();
+            inserir_fila(v);
+            break;
+        case 3:
+            printf("\n [ENFILEIRAR]");
+            printf("\n VALOR:");
+            fflush(stdin);
+            scanf("%d",&v);
+            inserir_fila(v);
+            break;
+        case 4:
+            printf("\n [DESENFILEIRAR]");
+            deletar_fila();
+            break;
+
+        }
+    }
+    while(opc != 5);
+
+
+
+
+    return 0;
+}
---
+++ 02.02_Pilha/02.Pilha/README.md
@@ -0,0 +1,3 @@
+
+
+![img](https://user-images.githubusercontent.com/33932398/98609965-4f5bfa00-22cd-11eb-87eb-6af971a60e67.png)
